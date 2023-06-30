6f0daf5909de32b34215bdc819893be430f81c19
Quordis <138236754+Quordis@users.noreply.github.com>
2023-06-30 17:44:29 +0200

    Add files via upload

---
+++ Zadanie1/Zadanie1/Osoba.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    internal class Osoba
+    {
+        public Osoba(string imięNazwisko)
+        {
+            ImięNazwisko = imięNazwisko;
+        }
+
+        string imię;
+        public string Nazwisko;
+        public DateTime? DataUrodzenia = null;
+        public DateTime? DataŚmierci = null;
+
+        public string Imię 
+        { 
+            get => imię;
+            set
+            {
+                if (value == string.Empty)
+                {
+                    Exception exception = new Exception("Pole nie może być puste");
+                    throw exception;
+                }
+                else
+                imię = value;
+            }
+        }
+        public string ImięNazwisko 
+        {
+            get
+            {
+                return $"{imię} {Nazwisko}";
+            }
+            set
+            {
+                var imienazwisko = value.Split(' ');
+                Imię = imienazwisko[0];
+                if (imienazwisko.Length == 1) 
+                {
+                    Nazwisko = string.Empty;
+                }
+                else
+                Nazwisko = imienazwisko[1];
+                
+            } 
+        }
+        
+        public TimeSpan? Wiek()
+        {
+           if (DataUrodzenia == null) return null;
+            if (DataŚmierci != null) 
+            {
+                return (DataŚmierci - DataUrodzenia)/365;
+            }
+            else
+            {
+                return (DateTime.Now - DataUrodzenia)/365;
+            }
+        }
+    }
+}
---
+++ Zadanie1/Zadanie1/Program.cs
@@ -0,0 +1,7 @@
+﻿using Zadanie1;
+
+Osoba p1 = new Osoba(" ");
+Console.WriteLine("Imię: {0}\nNazwisko: {1}\nImię i Nazwisko: {2}", p1.Imię, p1.Nazwisko, p1.ImięNazwisko);
+p1.DataUrodzenia = new DateTime(2003, 5, 12);
+p1.DataŚmierci = new DateTime(2023, 6, 21);
+Console.WriteLine("Czas od urodzenia {0}", p1.Wiek());
